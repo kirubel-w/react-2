201763173871f2aaf04c54ebe3e88cb0f65789e4
kirubel <kirabwill@yahoo.com>
2021-12-25 14:21:26 -0600

    added component

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Kirubel Woldeeyesus",
+    job: "web developer",
+    image:
+      // 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      "https://scontent.ffcm1-2.fna.fbcdn.net/v/t39.30808-6/262780617_2370686646399604_719309091724079776_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=3E4xmmHRyisAX8rQzTV&_nc_ht=scontent.ffcm1-2.fna&oh=00_AT8Q1pwUuwPm2-syL15UuKom6nYBgnNMG5GiU_UVABY-DQ&oe=61CB6CB9",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "steelo",
+    job: "web designer",
+    image:
+      // "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      "https://scontent.ffcm1-1.fna.fbcdn.net/v/t1.6435-9/105543627_1908404395961167_4451644249465073042_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=174925&_nc_ohc=6X5PS7d4rpcAX-GIHAt&_nc_oc=AQkwxN6rKaNOPdM4pTBHf_PyfL27A2PqsPP7gBlw-dAPYH274N8bkDT1zh7BMrUCKWTdkSQzCmEItqrkdku0Jt3_&_nc_ht=scontent.ffcm1-1.fna&oh=00_AT8ednG6rKUDl1B54sOvo-pLaWwH2qH4eE_F1MpD0DOXxg&oe=61EC5FD9",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "kiru",
+    job: "intern",
+    image:
+      // "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      "https://scontent.ffcm1-2.fna.fbcdn.net/v/t39.30808-6/262780617_2370686646399604_719309091724079776_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=3E4xmmHRyisAX8rQzTV&_nc_ht=scontent.ffcm1-2.fna&oh=00_AT8Q1pwUuwPm2-syL15UuKom6nYBgnNMG5GiU_UVABY-DQ&oe=61CB6CB9",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "k.w",
+    job: "the boss",
+    image:
+      // "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      "https://scontent.ffcm1-2.fna.fbcdn.net/v/t39.30808-6/p526x296/264160796_2370636599737942_8964528149418109678_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=nh43AxpZIeUAX9Hk_6K&tn=f_GMt2hGBqJbxL3O&_nc_ht=scontent.ffcm1-2.fna&oh=00_AT9ctBgVKdbjwTXEquLJqHQHp85FJnD_Nnq032EipTGpOw&oe=61CA52CF",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+export default reviews;
